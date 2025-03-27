1cfe322134f4d6d0f8d609e7beb32cda88369dbb
jithendra150 <jithendranathreddyc@gmail.com>
2025-03-27 21:21:59 +0530

    Update s3.bucket.tf

--- s3.bucket.tf
+++ s3.bucket.tf
@@ -1,8 +1,14 @@
 resource "aws_s3_bucket" "storage" {
-  bucket = "my-tf-test-bucket"
-
+  bucket = "my-tf-test-bucket-${random_string.suffix.result}"  # Unique name
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+# Add a random suffix to make the bucket name unique
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
 }
